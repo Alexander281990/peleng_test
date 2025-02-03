ec06c8dce3054c68a056212febf86f550073e504
AlexanderIvanov <alexivanov901128@gmail.com>
2025-02-04 01:35:49 +0300

    added input string in monitor port and output string width 7 char

--- peleng_test/peleng_test.ino
+++ peleng_test/peleng_test.ino
@@ -1,9 +1,27 @@
+#include <EEPROM.h>
+
 void setup() {
- 
+  Serial.begin(9600);
+
 
 }
 
 void loop() {
-  
+  if (Serial.available() > 0) {
+    String inputStr = Serial.readString();
+    inputStr.trim();
+    
+    if (inputStr.startsWith("eeprom ")) {
+      handleFunction(inputStr.substring(7));
+    } else {
+      Serial.println("Ошибка: Неверная команда");
+    }
+  }
 
+}
+
+void handleFunction(String str) {
+
+  Serial.print(str);
+  
 }
