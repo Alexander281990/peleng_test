1d454cce959ef38078662b1321ee08ce47f4c833
AlexanderIvanov <alexivanov901128@gmail.com>
2025-02-06 01:06:27 +0300

    added function writeEeprom

--- peleng_test/peleng_test.ino
+++ peleng_test/peleng_test.ino
@@ -16,6 +16,13 @@ void loop() {
         readEeprom(keyAddres, addres);
       } else if (key == "-e") {
         clearEeprom(keyAddres, addres);
+      } else if (key == "-w") {
+        int vIndex = inputStr.indexOf("-v"); // Узнаем с какой позиции начинается "-v"
+        String keyValue = inputStr.substring(vIndex, vIndex + 2);
+        int writeValue = inputStr.substring(vIndex + 3).toInt();
+        writeEeprom(keyAddres, addres, keyValue, writeValue);
+      } else {
+        Serial.println("Ошибка: Неверная команда");
       }
     } else {
       Serial.println("Ошибка: Неверная команда");
@@ -46,6 +53,19 @@ void clearEeprom(String s, int address) {
     Serial.print(address);
     Serial.print(": ");
     Serial.println("Успешно стерто");
+  } else {
+    Serial.println("Ошибка: Неверная команда");
+  }
+}
+
+// Функция для записи ячейки памяти
+void writeEeprom(String s, int address, String kV, int wV) {
+  if (s == "-a" && kV == "-v") {
+    EEPROM.write(address, wV);
+    Serial.print("Значение по адресу ");
+    Serial.print(address);
+    Serial.print(": ");
+    Serial.println("Успешно записано");
   } else {
     Serial.println("Ошибка: Неверная команда");
   }
