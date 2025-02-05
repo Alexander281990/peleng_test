7906851dbb50d6f5919f228b8e4edcdf15c6355d
AlexanderIvanov <alexivanov901128@gmail.com>
2025-02-05 23:43:58 +0300

    add readEeprom function

--- peleng_test/peleng_test.ino
+++ peleng_test/peleng_test.ino
@@ -2,26 +2,37 @@
 
 void setup() {
   Serial.begin(9600);
-
-
 }
 
 void loop() {
   if (Serial.available() > 0) {
     String inputStr = Serial.readString();
-    inputStr.trim();
-    
+    inputStr.trim(); // Убираем все пробелы в начале и конце строки
     if (inputStr.startsWith("eeprom ")) {
-      handleFunction(inputStr.substring(7));
+      String key = inputStr.substring(7, 9);
+      key.trim();
+      if (key == "-r") {
+        String keyAddres = inputStr.substring(10, 12);
+        keyAddres.trim();
+        int addres = inputStr.substring(13).toInt(); // Преобразуем строку в число
+        readEeprom(keyAddres, addres);
+      } else {
+        Serial.println("Ошибка: Неверная команда");
+      }
     } else {
       Serial.println("Ошибка: Неверная команда");
     }
   }
-
 }
 
-void handleFunction(String str) {
-
-  Serial.print(str);
-  
+void readEeprom(String s, int address) {
+  if (s == "-a") {
+    int value = EEPROM.read(address);
+    Serial.print("Значение по адресу ");
+    Serial.print(address);
+    Serial.print(": ");
+    Serial.println(value);
+  } else {
+    Serial.println("Ошибка: Неверная команда");
+  }
 }
